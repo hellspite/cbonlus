f34a78ed1b387a4ec3017dc2092beda1a336f00b
Emiliano Nobis <emiliano.nobis@gmail.com>
2016-11-17 11:35:36 +0100

    Inizio lavorazione pagina Equipe

--- src/AppBundle/Controller/EquipeController.php
+++ src/AppBundle/Controller/EquipeController.php
@@ -8,12 +8,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class EquipeController extends Controller
 {
     /**
-     * @Route("/equipe")
+     * @Route("/equipe", name="equipe")
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $equipe = $em->getRepository('AppBundle:Equipe')->findAll();
+
         return $this->render('AppBundle:Equipe:index.html.twig', array(
-            // ...
+            'equipe' => $equipe
         ));
     }
 

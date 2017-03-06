04fcae7f949b011b0eb5bbfbb6fbaf0448dfacaf
Emiliano Nobis <emiliano.nobis@gmail.com>
2017-03-06 23:16:58 +0100

    Inizio lavorazione pagina Servizi Integrativi

--- src/AppBundle/Controller/ServiceController.php
+++ src/AppBundle/Controller/ServiceController.php
@@ -4,16 +4,21 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Service;
 
 class ServiceController extends Controller
 {
     /**
-     * @Route("servizi-integrativi")
+     * @Route("servizi-integrativi", name="service_index")
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $services = $em->getRepository('AppBundle:Service')->findAll();
+
         return $this->render('AppBundle:Service:index.html.twig', array(
-            // ...
+            'services' => $services
         ));
     }
 
